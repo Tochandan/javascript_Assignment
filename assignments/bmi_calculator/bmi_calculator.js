f65559187f5d89c84cbc5fd878b37f4ccdad0323
Tochandan <56221686+Tochandan@users.noreply.github.com>
2021-11-27 23:44:56 +0530

    variable declaratin

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -3,8 +3,10 @@ function BMICalculator(mass, height) {
     // Write your code here
     if (height===0 || mass===0){return "INVALID INPUT"}
 
-    var BMI = mass / (height * height);
+    var BMI;
+    BMI = mass / (height * height);
     return BMI
+// end of code 
 }
 
 module.exports = BMICalculator;
